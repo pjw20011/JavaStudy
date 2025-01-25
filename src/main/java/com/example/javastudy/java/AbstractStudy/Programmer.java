9baf726def27114de4292d96e75793d107992ecf
Jeongwon Park <pjw200116@gmail.com>
2025-01-25 22:05:16 +0900

    AbstractStudy

---
+++ src/main/java/com/example/javastudy/java/AbstractStudy/Programmer.java
@@ -0,0 +1,9 @@
+package com.example.javastudy.java.AbstractStudy;
+
+abstract public class Programmer {
+    abstract void writeCode();
+
+    void introduce(){
+        System.out.println("something");
+    }
+}
